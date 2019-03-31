2ae3558701be03df4d469880d08306c7af5bd4b8
rosealin <1019766878@qq.com>
2019-03-31 20:28:00 +0800

    first commit

---
+++ src/components/sub/confirm.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="confirm">
+        <button @click="getbuttonclick">{{text || '确认'}}</button>
+    </div>
+</template>
+<script>
+export default {
+    name:'confirm',
+    props:['text'],
+    data(){
+        return {
+            msg:true
+        }
+    },
+    methods:{
+        getbuttonclick(){
+            this.$emit("message",this.msg);
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.confirm{
+    font-size: 30px;
+    .link{
+        a{
+            color: red;
+        }
+    }
+}
+</style>
+
+
---
+++ src/components/views/First.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="first">
+        {{msg}}
+        <confirm text="注册" @message="getMessage"></confirm>
+        <p><router-link to="/second">去第二个页面</router-link></p>
+    </div>
+</template>
+<script>
+import confirm from '../sub/confirm'
+export default {
+    name:'First',
+    components:{
+        confirm
+    },
+    data(){
+        return{
+            msg:'welcome to firstApp'
+        }
+    },
+    methods:{
+        getMessage(val){
+            alert(val);
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+.first{
+    font-size: 30px;
+    a{
+        color: red;
+        text-decoration: none;
+    }
+}
+</style>
+
+
+
